33b7c7e1bd8b93d53f9ff4e35da53c5efdd1d060
Arcensoth <arcensoth@gmail.com>
2021-10-29 13:12:57 -0400

    Add a flask of precursor for fun

--- gold_to_porridge/init.lua
+++ gold_to_porridge/init.lua
@@ -8,6 +8,8 @@ function OnWorldInitialized()
 		local from_material_id = CellFactory_GetType(it)
 		ConvertMaterialEverywhere(from_material_id, to_material_id)
 	end
+
+	local entity = EntityLoad("mods/gold_to_porridge/files/potion_precursor.xml", 240, -80)
 end
 
 function main()
